4b34589b6620f800efebed927f4cdd34589982f1
Safayet Hossain <emonsafayet@gmail.com>
2024-05-28 01:28:30 +0600

    .

--- Restaurant.Application/Restaurants/Dtos/RestaurantsProfile.cs
+++ Restaurant.Application/Restaurants/Dtos/RestaurantsProfile.cs
@@ -1,33 +1,30 @@
-﻿//using AutoMapper;
-//using Restaurants.Application.Restaurants.Commands.CreateRestaurant;
-//using Restaurants.Application.Restaurants.Commands.UpdateRestaurant;
-//using Restaurants.Domain.Entities;
-//using static System.Runtime.InteropServices.JavaScript.JSType;
+﻿using AutoMapper;
+using Restaurants.Domain.Entities;
 
-//namespace Restaurants.Application.Restaurants.Dtos;
+namespace Restaurants.Application.Restaurants.Dtos;
 
-//public class RestaurantsProfile : Profile
-//{
-//    public RestaurantsProfile()
-//    {
-//        CreateMap<UpdateRestaurantCommand, Restaurant>();
+public class RestaurantsProfile : Profile
+{
+    public RestaurantsProfile()
+    {
+        //CreateMap<UpdateRestaurantCommand, Restaurant>();
 
-//        CreateMap<CreateRestaurantCommand, Restaurant>()
-//            .ForMember(d => d.Address, opt => opt.MapFrom(
-//                src => new Address
-//                {
-//                    City = src.City,
-//                    PostalCode = src.PostalCode,
-//                    Street = src.Street
-//                }));
+        //CreateMap<CreateRestaurantCommand, Restaurant>()
+        //    .ForMember(d => d.Address, opt => opt.MapFrom(
+        //        src => new Address
+        //        {
+        //            City = src.City,
+        //            PostalCode = src.PostalCode,
+        //            Street = src.Street
+        //        }));
 
-//        CreateMap<Restaurant, RestaurantDto>()
-//            .ForMember(d => d.City, opt =>
-//                opt.MapFrom(src => src.Address == null ? null : src.Address.City))
-//            .ForMember(d => d.PostalCode, opt =>
-//                opt.MapFrom(src => src.Address == null ? null : src.Address.PostalCode))
-//            .ForMember(d => d.Street, opt =>
-//                opt.MapFrom(src => src.Address == null ? null : src.Address.Street))
-//            .ForMember(d => d.Dishes, opt => opt.MapFrom(src => src.Dishes));
-//    }
-//}
+        CreateMap<Restaurant, RestaurantDto>()
+            .ForMember(d => d.City, opt =>
+                opt.MapFrom(src => src.Address == null ? null : src.Address.City))
+            .ForMember(d => d.PostalCode, opt =>
+                opt.MapFrom(src => src.Address == null ? null : src.Address.PostalCode))
+            .ForMember(d => d.Street, opt =>
+                opt.MapFrom(src => src.Address == null ? null : src.Address.Street))
+            .ForMember(d => d.Dishes, opt => opt.MapFrom(src => src.Dishes));
+    }
+}
