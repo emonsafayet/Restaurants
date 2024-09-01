882baa3f66a8cc2582b9e67612878f5f8feccb63
Safayet Hossain <emonsafayet@gmail.com>
2024-09-01 18:32:06 +0600

    Custome modified claims.

--- Restaurant.Infrastructure/Extensions/ServiceCollectionExtensions.cs
+++ Restaurant.Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Restaurants.Domain.Entities;
 using Restaurants.Domain.Repositories;
+using Restaurants.Infrastructure.Authorization;
 using Restaurants.Infrastructure.Persistance;
 using Restaurants.Infrastructure.Repositories;
 using Restaurants.Infrastructure.Seeders;
@@ -12,15 +13,18 @@ namespace Restaurants.Infrastructure.Extensions;
 
 public static class ServiceCollectionExtensions
 {
-    public static void AddInfrastructure(this IServiceCollection services, IConfiguration  configuration)
+    public static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
     {
         var connectionString = configuration.GetConnectionString("RestaurantsDb");
-        services.AddDbContext<RestaurantsDbContext>(opt=>opt.UseSqlServer(connectionString).EnableSensitiveDataLogging());
+        services.AddDbContext<RestaurantsDbContext>(opt => opt.UseSqlServer(connectionString).EnableSensitiveDataLogging());
+      
         services.AddIdentityApiEndpoints<User>()
             .AddRoles<IdentityRole>()
-            .AddEntityFrameworkStores<RestaurantsDbContext>();        
-        services.AddScoped<IRestaurantsSeeder,RestaurantsSeeder>();
-        services.AddScoped<IRestaurantsRepository,RestaurantsRepository>();
-        services.AddScoped<IDishesRepository,DishesRepository>();
+            .AddClaimsPrincipalFactory<RestaurantsUserClaimsPrincipalFactory>()
+            .AddEntityFrameworkStores<RestaurantsDbContext>();
+
+        services.AddScoped<IRestaurantsSeeder, RestaurantsSeeder>();
+        services.AddScoped<IRestaurantsRepository, RestaurantsRepository>();
+        services.AddScoped<IDishesRepository, DishesRepository>();
     }
 }
--- Restaurants.API/Controllers/IdentityController.cs
+++ Restaurants.API/Controllers/IdentityController.cs
@@ -2,6 +2,9 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Restaurants.Application.Users.Commands.UpdateUserDetails;
+using Restaurants.Application.Users.Commands.AssignUserRole;
+using Restaurants.Application.Users.Commands.UnassignUserRole;
+using Restaurants.Domain.Constants;
 using System.Threading.Tasks;
 
 namespace Restaurants.API.Controllers;
@@ -19,19 +22,19 @@ public class IdentityController(IMediator mediator) : ControllerBase
     }
 
 
-    //[HttpPost("userRole")]
-    //[Authorize(Roles = UserRoles.Admin)]
-    //public async Task<IActionResult> AssignUserRole(AssignUserRoleCommand command)
-    //{
-    //    await mediator.Send(command);
-    //    return NoContent();
-    //}
+    [HttpPost("userRole")]
+    [Authorize(Roles = UserRoles.Admin)]
+    public async Task<IActionResult> AssignUserRole(AssignUserRoleCommand command)
+    {
+        await mediator.Send(command);
+        return NoContent();
+    }
 
-    //[HttpDelete("userRole")]
-    //[Authorize(Roles = UserRoles.Admin)]
-    //public async Task<IActionResult> UnassignUserRole(UnassignUserRoleCommand command)
-    //{
-    //    await mediator.Send(command);
-    //    return NoContent();
-    //}
+    [HttpDelete("userRole")]
+    [Authorize(Roles = UserRoles.Admin)]
+    public async Task<IActionResult> UnassignUserRole(UnassignUserRoleCommand command)
+    {
+        await mediator.Send(command);
+        return NoContent();
+    }
 }
